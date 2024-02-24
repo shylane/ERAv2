8b7292226a8bc678eab87c3d46a131b0e521cb1b
shylane <dshailenk@gmail.com>
2024-02-24 08:44:24 +0530

    adding code and readme

---
+++ utils.py
@@ -0,0 +1,90 @@
+import torch
+from torch import optim
+import torch.nn.functional as F
+from torchvision import datasets, transforms
+from tqdm import tqdm
+
+# Train data transformations
+def train_data():
+    train_transforms= transforms.Compose([
+        transforms.RandomApply([transforms.CenterCrop(22), ], p=0.1),
+        transforms.Resize((28, 28)),
+        transforms.RandomRotation((-15., 15.), fill=0),
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,)),
+        ])
+    return datasets.MNIST('../data', train=True, download=True, transform=train_transforms)
+
+
+# Test data transformations
+def test_data():
+    test_transforms= transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+        ])
+    return datasets.MNIST('../data', train=False, download=True, transform=test_transforms)
+
+def criterion():
+    return F.nll_loss
+
+def optimizer(model):
+    return optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+
+def scheduler(model):
+    return optim.lr_scheduler.StepLR(optimizer(model), step_size=15, gamma=0.1, verbose=True)
+
+def GetCorrectPredCount(pPrediction, pLabels):
+    return pPrediction.argmax(dim=1).eq(pLabels).sum().item()
+
+def train(model, device, train_loader, optimizer, criterion, train_losses, train_acc):
+    model.train()
+    pbar = tqdm(train_loader)
+
+    train_loss = 0
+    correct = 0
+    processed = 0
+
+    for batch_idx, (data, target) in enumerate(pbar):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+
+        # Predict
+        pred = model(data)
+
+        # Calculate loss
+        loss = criterion(pred, target)
+        train_loss+=loss.item()
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+        
+        correct += GetCorrectPredCount(pred, target)
+        processed += len(data)
+
+        pbar.set_description(desc= f'Train: Loss={loss.item():0.4f} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
+
+    return train_acc + [100*correct/processed], train_losses + [train_loss/len(train_loader)]
+
+def test(model, device, test_loader, criterion, test_losses, test_acc):
+    model.eval()
+
+    test_loss = 0
+    correct = 0
+
+    with torch.no_grad():
+        for batch_idx, (data, target) in enumerate(test_loader):
+            data, target = data.to(device), target.to(device)
+
+            output = model(data)
+            test_loss += criterion(output, target, reduction='sum').item()  # sum up batch loss
+
+            correct += GetCorrectPredCount(output, target)
+
+
+    test_loss /= len(test_loader.dataset)
+    
+    print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+    return test_acc.append(100. * correct / len(test_loader.dataset)), test_losses.append(test_loss)
