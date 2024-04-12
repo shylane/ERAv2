058d71dd26e413e7a13673af018280133673b0f0
Shailen Kumar Dalbehera <shailen.k.dalbehera@in.ibm.com>
2024-04-12 13:41:38 +0530

    cleaning up to submit

--- s10_custom_resnet_cifar10/README.md
+++ s10_custom_resnet_cifar10/README.md
@@ -7,7 +7,7 @@ The code is organized into separate files to promote modularity and reusability.
 
 - `model.py`: This file contains the neural network architecture.
 - `utils.py`: This file contains utility functions for data loading, training and evaluation.
-- `S5.ipynb`: This Jupyter notebook contains the code for training and testing the CNN.
+- `S10.ipynb`: This Jupyter notebook contains the code for training and testing the CNN. This can be run on Collab.
 - `README.md`: This file (provides instructions and overview).
 
 ## Requirements
@@ -16,7 +16,7 @@ The code is organized into separate files to promote modularity and reusability.
 - PyTorch 1.x
 - torchvision
 - torchsummary
-- pytorch-lr-finder
+- torch-lr-finder
 - tensorboard
 - matplotlib
 - tqdm
@@ -25,14 +25,14 @@ The code is organized into separate files to promote modularity and reusability.
 
 1. Clone this repository.
 2. Install required libraries: `pip install torch torchvision torchsummary matplotlib tqdm torch-lr-finder`
-3. Open the Jupyter Notebook `S11.ipynb` and follow the instructions within the notebook to train and test the CNN, and see the summary.
+3. Open the Jupyter Notebook `S10.ipynb` and follow the instructions within the notebook to train and test the CNN, and see the summary.
 
 ## Key Features
 
 * Data augmentation for training data.
 * Clear separation of model architecture and training logic.
-* Visual tracking of training and validation accuracy and loss.
-* Optional model summary using `torchsummary`.
+* Tracking of training and test accuracy and loss using tensorboard.
+* Mmodel summary using `torchsummary`.
 
 ## License
 
--- s10_custom_resnet_cifar10/utils.py
+++ s10_custom_resnet_cifar10/utils.py
@@ -4,7 +4,6 @@ from torchvision.transforms import v2
 from torch_lr_finder import LRFinder
 from tqdm import tqdm
 import numpy
-# from torch.optim.lr_scheduler import OneCycleLR
 
 # Train data transformations
 def train_data():
@@ -34,6 +33,7 @@ def find_lr(train_loader,test_loader,device,model,optimizer,criterion):
     lr_finder.range_test(train_loader, val_loader=test_loader, end_lr=10, num_iter=100, step_mode="exp")
     lr_finder.plot(log_lr=False,skip_end=0)
     lr_finder.reset()
+    
 # Cutout implementation by David Stutz
 # Refer https://davidstutz.de/2-percent-test-error-on-cifar10-using-pytorch-autoagument/
 class CutoutAfterToTensor(object):
@@ -60,79 +60,3 @@ class CutoutAfterToTensor(object):
         img = img * mask + (1 - mask) * self.fill_color[:, None, None]
         return img
 
-# # One Cycle Policy
-# def one_cycle_policy(optimizer, epochs, max_at_epoch, steps, lr_min, lr_max):
-#     scheduler = OneCycleLR(
-#         optimizer,
-#         max_lr=lr_max,
-#         steps_per_epoch=steps,
-#         epochs=epochs,
-#         pct_start=max_at_epoch / epochs
-#     )
-#     return scheduler
-
-
-# def criterion():
-#     return F.nll_loss
-
-# def optimizer(model):
-#     return optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
-
-# def scheduler(model):
-#     return optim.lr_scheduler.StepLR(optimizer(model), step_size=15, gamma=0.1, verbose=True)
-
-# def GetCorrectPredCount(pPrediction, pLabels):
-#     return pPrediction.argmax(dim=1).eq(pLabels).sum().item()
-
-def train(model, device, train_loader, optimizer, criterion, train_losses, train_acc):
-    model.train()
-    pbar = tqdm(train_loader)
-
-    train_loss = 0
-    correct = 0
-    processed = 0
-
-    for batch_idx, (data, target) in enumerate(pbar):
-        data, target = data.to(device), target.to(device)
-        optimizer.zero_grad()
-
-        # Predict
-        pred = model(data)
-
-        # Calculate loss
-        loss = criterion(pred, target)
-        train_loss+=loss.item()
-
-        # Backpropagation
-        loss.backward()
-        optimizer.step()
-        
-        correct += GetCorrectPredCount(pred, target)
-        processed += len(data)
-
-        pbar.set_description(desc= f'Train: Loss={loss.item():0.4f} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
-
-    return train_acc + [100*correct/processed], train_losses + [train_loss/len(train_loader)]
-
-def test(model, device, test_loader, criterion, test_losses, test_acc):
-    model.eval()
-
-    test_loss = 0
-    correct = 0
-
-    with torch.no_grad():
-        for batch_idx, (data, target) in enumerate(test_loader):
-            data, target = data.to(device), target.to(device)
-
-            output = model(data)
-            test_loss += criterion(output, target, reduction='sum').item()  # sum up batch loss
-
-            correct += GetCorrectPredCount(output, target)
-
-
-    test_loss /= len(test_loader.dataset)
-    
-    print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
-    return test_acc+[100. * correct / len(test_loader.dataset)], test_losses+[test_loss]
